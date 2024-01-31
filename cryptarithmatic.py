4751d2799fd9ea8150da9271e623e677e26c117a
Robert Lutz <robertlutzi@gmail.com>
2024-01-31 14:57:07 -0500

    introduce regular expression matching

--- cryptarithmatic.py
+++ cryptarithmatic.py
@@ -1,16 +1,22 @@
 # import string     # not clear if this is necessary
+import re
 
 
-def valid(expression):
-    subsexpession = expression.translate(table)
+def valid(expression, trans_table):
+    """ String {N:N} -> Maybe String
+    returns the expression with digits substituted for variables in the case that 
+    it forms a true 
+    and valid mathematical expression"""
+    subsexpression = expression.translate(trans_table)
     try:
-         if eval(subsexpession):
-             return subsexpession
-    except:
-        ArithmeticError
+         if not(re.search(r'\b0[0-9]', subsexpression)) and eval(subsexpression):
+             return subsexpression
+    except ArithmeticError:
+        return False
         
 
 
-table = str.maketrans('ABC', '123')
-f = 'A + B == C'
+table = str.maketrans('ABCD', '1230')
+f = 'A + B - C == D'
+print(table)
 print(valid(f))
